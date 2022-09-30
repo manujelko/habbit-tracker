2a483592ed74b9a442b6690b501af36014610a31
Manuel Šarić <manujelko@gmail.com>
2022-09-30 16:43:05 +0200

    Using MongoDB

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask
+from pymongo import MongoClient
 
 from routes import pages
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(pages)
+    client = MongoClient(os.getenv("MONGODB_URI"))
+    app.db = client.get_default_database()
 
+    app.register_blueprint(pages)
     return app
