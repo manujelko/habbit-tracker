cfbbd49b4e24f94bd9eb90bc683f8766f353fa47
Manuel Šarić <manujelko@gmail.com>
2022-09-30 16:21:49 +0200

    Adding Flask blueprints

--- app.py
+++ app.py
@@ -1,53 +1,10 @@
-import datetime
-from collections import defaultdict
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 
-app = Flask(__name__)
-habits = []
-completions = defaultdict(list)
-
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-        return dates
-    
-    return {"date_range": date_range}
+from routes import pages
 
 
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages)
 
-    return render_template(
-        "index.html.j2",
-        title="Habit Tracker - Home",
-        habits=habits,
-        selected_date=selected_date,
-        completions=completions[selected_date],
-    )
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habits.append(request.form.get("habit"))
-    return render_template(
-        "add_habit.html.j2",
-        title="Habit Tracker - Add Habit",
-        selected_date=datetime.date.today(),
-        )
-
-
-@app.route("/complete", methods=["POST"])
-def complete():
-    date_string = request.form.get("date")
-    habit = request.form.get("habitName")
-    date = datetime.date.fromisoformat(date_string)
-    completions[date].append(habit)
-
-    return redirect(url_for("index", date=date_string))
+    return app
